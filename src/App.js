1c8fd50bd8f7db06b3cedd7bf18f5127e1ecd256
Thomas Mei <thomas.mei@gmail.com>
2018-11-13 16:17:37 -0500

    Commit # 3 - Markers added to Map with Info Window functionality

--- src/App.js
+++ src/App.js
@@ -7,22 +7,58 @@ import Menu from './components/menu.js'
 var google;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations : [
+      {title: 'Ippudo NY', location: {lat: 40.730948, lng: -73.990287}},
+      {title: 'Totto Ramen', location: {lat: 40.764422, lng: -73.987552}},
+      {title: 'Momofuku Noodle Bar', location: {lat: 40.729233, lng: -73.98451}},
+      {title: 'Minca', location: {lat: 40.723998, lng: -73.982949 }},
+      {title: 'Momosan Ramen & Sake', location: {lat: 40.749926, lng: -73.977}}
+    ]
+  }
+}
+
   componentDidMount() {
     window.initMap = this.initMap;
+    window.state = this.state;
+    console.log(this.state.locations[0].location.lat);
+    console.log(this.state.locations[0].location.lng);
     loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyB-mwNzKac0ksFHUQAFD5HXTbYmvrtBIrg&v=3&callback=initMap')
 }
 
   initMap() {
+    console.log(this.state);
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.741328, lng: -74.003247},
-      zoom: 13
+      center: {lat: 40.740775, lng: -73.983935},
+      zoom: 14
     });
+
+      this.state.locations.map(locale => {
+        const marker = new window.google.maps.Marker({
+        position: {lat: locale.location.lat, lng: locale.location.lng},
+        map: map,
+        title: locale.title
+      });
+
+      const infowindow = new window.google.maps.InfoWindow({
+        content: marker.title
+  });
+
+        marker.addListener('click', function(){
+          infowindow.open(map, marker)
+        })
+      })
+
   }
+
+
 
   render() {
     return (
       <main>
-        <Menu />
+        <Menu title={this.state.locations}/>
         <div id="map"></div>
       </main>
 
--- src/components/menu.js
+++ src/components/menu.js
@@ -2,26 +2,21 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../App.css';
 
-const locations = [
-  {title: 'Ippudo NY', location: {lat: 40.730948, lng: 73.990287}},
-  {title: 'Totto Ramen', location: {lat: 40.764422, lng: 73.987552}},
-  {title: 'Momofuku Noodle Bar', location: {lat: 40.729233, lng: -73.98451}},
-  {title: 'Minca', location: {lat: 40.723998, lng: -73.982949 }},
-  {title: 'Momosan Ramen & Sake', location: {lat: 40.749926, lng: -73.977445}}
-]
-
 class Menu extends Component {
   render() {
-    console.log(this);
+//    const locations = this.props.locations;
+    console.log(this.props.title);
+//    console.log(locations[0]);
+
     return (
       <div id="menu">
         <p>Menu</p>
         <ul>
-        {
-          locations.map(function(title, i){
-            return <li key={i}>{locations[i].title}</li>
-          })
-        }
+          <li>{this.props.title[0].title}</li>
+          <li>{this.props.title[1].title}</li>
+          <li>{this.props.title[2].title}</li>
+          <li>{this.props.title[3].title}</li>
+          <li>{this.props.title[4].title}</li>
         </ul>
       </div>
     );
